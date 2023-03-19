afd5cf73417280e0133f34931a9c029a34267f3f
zukardex <zenzenram@gmail.com>
2023-03-19 12:28:11 +0530

    somewhat stable
    Co-Authored-By: Sreyz03 <127430568+Sreyz03@users.noreply.github.com>
    Co-Authored-By: Akarsh-J-Kalathil <123386068+Akarsh-J-Kalathil@users.noreply.github.com>
    Co-Authored-By: HANANJASIM <127536839+HANANJASIM@users.noreply.github.com>

--- submitLoc.php
+++ submitLoc.php
@@ -1,15 +1,43 @@
 <?php
-if(isset($_GET['nmbr']) &&isset($_GET['networks']) && isset($_GET['strengths'])){
+
+include 'test.php';
+
+
+
+
+if( isset($_POST['nmbr']) && isset($_POST['networks']) && isset($_POST['strengths']) ) {
     //nmbr= number of wifi networks available
     //networks= names of wifi networks seperated by a comma(%2C)
     //strengths= strengths of the available WiFi networks
+
     
-    //counting networks
-    networks= explode($_GET['networks'], '%2C');
-    strengths= explode($_GET['strengths'], '%2C');
-    if(count(networks) == count(strengths)){
-        //matches
+    $networks= explode( ',', $_POST['networks']);
+    $strengths=explode( ',', $_POST['strengths']);
+    if(count($strengths) >=2){ //for comparing
         
+        //strengrths casting to int and finding the largest int
+        $first= (((int) $strengths[0]) > ((int) $strengths[1])) ? ((int) $strengths[0]) : ((int) $strengths[1]);
+        $second= (((int) $strengths[0]) > ((int) $strengths[1])) ? ((int) $strengths[1]) : ((int) $strengths[0]);
+        foreach($strengths as $st){ 
+            $istrength= (int) $st;
+            if($istrength > $first){               
+                $second= $first;
+                $first = $istrength;
+            }
+        }
+        $rooms[0]= $networks[array_search($first, $strengths)];
+        $rooms[1]= $networks[array_search($second, $strengths)];
+        $str= $rooms[0] . $rooms[1];
+        $common=''; $common2='';
+        for($i=0; $i< strlen($str); $i++){
+            $common2=$str[$i];
+            for($j=$i +1 ; $j< strlen($str); $j++){
+                if($common2 == $str[$j]){
+                    $common = $common2;
+                }
+            }
+        }
+        echo $common; // $common returns the room name
     }
 
 }
---
+++ test.php
@@ -0,0 +1,11 @@
+<form method="post"> <!--  action="submitLoc.php">-->
+<input type="text" name="nmbr" value="14" ><br>
+<input type="text" name="networks" value="AA, AB, BC, CD, DE, EF, FG, GH, HI, IJ, JK ,KL, LM, MM"><br>
+<input type="text" name="strengths" value="-100, -98, -93, -90, -87, -79, -78, -76, -75, -79, -85, -85, -89, -101"><br>
+<input type="submit" name="submit">
+</form>
+<?php
+$roomnames= array('Storeroom'=> 'A', 'Canteen'=> 'B', 'Washroom'=> 'C', 'Casuality'=>'D', 'Paediatric'=>'E', 'Orthology'=>'F', 'Cardiology'=>'G', 'Nephrology'=> 'H', 'Neurology'=>'I', 'Ophthalmlogy'=>'J', 'Urology'=>'K', 'ICU'=>'L', 'Mortuary'=>'M');
+
+
+?>
